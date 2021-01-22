37e3cb1666b17a32ffe6ac0d07d7df1b50e50f1d
CTY <1959136770@qq.com>
2021-01-22 11:42:01 +0800

    新增了baseAPI.js文件,优化代码

--- assets/js/login.js
+++ assets/js/login.js
@@ -31,7 +31,7 @@ $(function(){
         e.preventDefault();
         $.ajax({
             method: 'POST',
-            url: 'http://api-breakingnews-web.itheima.net/api/reguser',
+            url: '/api/reguser',
             data: {
                 username: $('.reg-box [name=username]').val(),
                 password: $('.reg-box [name=password]').val()
@@ -52,7 +52,7 @@ $(function(){
         e.preventDefault();
         $.ajax({
             method: 'POST',
-            url: 'http://api-breakingnews-web.itheima.net/api/login',
+            url: '/api/login',
             data: $(this).serialize(),
             success: function(res){
                 if(res.status !== 0){
